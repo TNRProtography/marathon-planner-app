212b047ca8b70d4f9d676a257e99b4a85848dd5c
TNRProtography <thenamesrock@gmail.com>
2025-05-31 17:39:00 +1200

    // Marathon App/postcss.config.js

---
+++ Marathon App/postcss.config.js
@@ -0,0 +1,10 @@
+// Marathon App/postcss.config.js
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwindcss, // Use the imported variable
+    autoprefixer, // Use the imported variable
+  ],
+};
