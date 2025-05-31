3d03d4f0b043319cb054ca7a5c4ec3f1f3eb135f
TNRProtography <thenamesrock@gmail.com>
2025-05-31 17:41:26 +1200

    taaaaaiilllllwinnnndddssssss

--- Marathon App/tailwind.config.js
+++ Marathon App/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",                   
-    "./App.tsx",                      
-    "./index.tsx",                  
-    "./components/**/*.{js,ts,jsx,tsx}", 
+    "./index.html",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./index.css", // <--- ENSURE THIS LINE IS PRESENT
   ],
   theme: {
     extend: {},
